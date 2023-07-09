0ebfd4085393956fbc70969fedb4a42ef693c35a
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2023-07-09 13:05:00 -0500

    Se probó todas las acciones de cada modelo de campus diverso, tanto desde el lado de la interfaz del admin como en las peticiones HTTP al servidor, con resultado existoso (sólo falta poner unique=True en los nombres de los modelos diccionarios para evitar errores con la creación individual de datos iguales)

--- back-end/modulo_campus_diverso/models.py
+++ back-end/modulo_campus_diverso/models.py
@@ -297,7 +297,7 @@ class Estamento(models.Model):
         db_table = "campus_diverso_estamento"
     
     def __str__(self):
-        return f"InformacionAcademica: {self.id_informacion_academica}"
+        return f"Estamento: {self.nombre_estamento}"
 
 class InformacionAcademica(models.Model):
     id_informacion_academica = models.AutoField(primary_key=True)
@@ -307,7 +307,7 @@ class InformacionAcademica(models.Model):
     nombre_programa_academico = models.CharField(max_length=100)
     codigo_estudiante = models.CharField(max_length=20, unique=True)
     semestre_academico = models.IntegerField()
-    estamentos = models.ManyToManyField(Estamento, max_length=100)
+    estamentos = models.ManyToManyField(Estamento, max_length=100, related_name="id_informacion_academica")
     
     class Meta:
         db_table = "campus_diverso_informacion_academica"
